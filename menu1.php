e3876a1e80e5a6c0455e57e5a9efac006cd6ceb4
kocHENGG <magfirohanis23@gmail.com>
2023-05-08 20:49:49 +0700

    up

---
+++ menu1.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>KUIS</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-uWxY/CJNBR+1zjPWmfnSnVxwRheevXITnMqoEIeG1LJrdI0GlVs/9cVSyPYXdcSF" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="kuis.css">
+</head>
+<body>
+		<div class="container">
+			<nav class="navbar navbar-expand-lg navbar-light bg-light">
+			  <div class="container-fluid">
+			    <a class="navbar-brand" href="#">Kuis</a>
+			    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+			      <span class="navbar-toggler-icon"></span>
+			    </button>
+			    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+			      <div class="navbar-nav">
+			        <a class="nav-link active" aria-current="page" href="menu1.php">Home</a>
+			        <a class="nav-link" href="menu2.php">Form</a>
+			        <a class="nav-link" href="menu3.php">Pengulangan</a>
+			      </div>
+			    </div>
+			  </div>
+			</nav>
+			<div class="jaraktepi">
+		 <div class="jarak">
+	        <h1>Story of Jogja</h1>
+	   
+		<div class="row">
+		  	<div class="col-4">
+		  		<img src="jogja.jpeg" style="width:360px; height: 375px;">
+		  	</div>
+		  	<div class="col-6">
+	  		<p style="text-indent: 45px; text-align: left;">
+	  			Ketika Anda mendengar nama Yogyakarta, Anda akan berpikir tentang keunikan kota di Jawa di Indonesia ini. Kota ini disebut unik karena kita bisa melihat berbagai tradisi Jawa yang begitu melekat di kota ini dan masyarakatnya, seperti batik (kain dengan corak khusus yang dibuat dengan tangan (dilukis dan dicetak), kerajinan perak, pertunjukan wayang, musik tradisional atau gamelan, bahkan makanan khas Yogyakarta yang disebut Gudeg! yaitu sayuran yang terbuat dari buah nangka muda.
+	  		</p>
+	  		<p style="text-indent: 45px; text-align: left;">
+	  			Yogyakarta juga merupakan Ibukota Daerah Istimewa Yogyakarta di Jawa yang terkenal dengan Istana Sultan Yogyakarta atau Keraton Yogyakarta dimana keluarga Sultan masih tinggal di istana tersebut sampai sekarang . Istana ini juga terbuka untuk umum tetapi tidak semua bangunan, hanya beberapa bagian bangunan yang bisa dilihat oleh masyarakat. Banyak bagian dari kota ini juga dihiasi dengan sentuhan budaya Jawa di mana orang tidak akan pernah melupakannya dan selalu ingat karakteristik yang ada.
+	  		</p>
+	  		
+	  	</div>
+	  </div>
+	  	<p style="text-indent: 45px; text-align: left;">
+	  		 Ornamen-ornamen khas Jawa dapat kita temui di lampu-lampu jalananan , di bangunan atau gedung-gedung di sepanjang jalan dan lain-lain yang membuat kota ini menjadi unik dan cantik. Itulah mengapa kota ini disebut Culture City.
+	  	</p>
+	  	<p style="text-indent: 45px; text-align: left;">
+	  		Yogyakarta juga dikenal sebagai Kota Pelajar karena banyak terdapat Universitas, termasuk universitas terkemuka yaitu Universitas Gadjah Mada . Hal inilah yang membuat kota ini memiliki jumlah mahasiswa yang besar, yang berasal tidak hanya dari area Jawa tetapi juga dari luar Jawa , bahkan dari luar Indonesia, sehingga kota ini juga disebut sebagai salah satu kota yang heterogen di Indonesia
+	  	</p>
+	</div>
+	</div>
+
+	<div class="container">
+			<nav class="navbar navbar-expand-lg navbar-light bg-light">
+			  <div class="container-fluid">
+			  	<div class="tengah">
+			    <a class="navbar-brand" href="#">@2021</a>
+			    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+			      <span class="navbar-toggler-icon"></span>
+			    </button>
+			    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+			      <div class="navbar-nav">
+			     </div>
+	</div>
+
+
+</body>
+</html>
